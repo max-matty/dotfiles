176f6ca96cf8e5b2924e2a98a0bccc23ac6cbf0a
Massimo Fontana <maxmatty@gmail.com>
2024-01-04 17:50:55 +0100

    nvim-dap for debugging

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,57 @@
+return {
+	"mfussenegger/nvim-dap",
+
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"theHamsta/nvim-dap-virtual-text",
+	},
+
+	config = function()
+		local dap = require("dap")
+		dap.adapters.cppdbg = {
+			id = "cppdbg",
+			type = "executable",
+			command = "/home/max/.config/local/share/nvim/mason/bin/OpenDebugAD7",
+		}
+		dap.configurations.c = {
+			{
+				name = "Launch file",
+				type = "cppdbg",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtEntry = true,
+			},
+			{
+				name = "Attach to gdbserver :1234",
+				type = "cppdbg",
+				request = "launch",
+				MIMode = "gdb",
+				miDebuggerServerAddress = "localhost:1234",
+				miDebuggerPath = "/usr/bin/gdb",
+				cwd = "${workspaceFolder}",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+			},
+		}
+
+		local dapui = require("dapui")
+		dapui.setup()
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
+
+		require("nvim-dap-virtual-text").setup({
+			highlight_changed_variables = true,
+		})
+	end,
+}
